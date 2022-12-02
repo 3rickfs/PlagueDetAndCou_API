5f8f9d67a65cd321b97d10cc103256987f1f833f
Erick Fiestas Sorogastua <efiestass@upao.edu.pe>
2022-12-01 22:53:21 -0500

    fix: completar cambios segun indicaciones
    Considerar los comentarios en mayusculas para tener en cuenta

--- PlagueDetAndCouAPI/api/RadonDetCou.py
+++ PlagueDetAndCouAPI/api/RadonDetCou.py
@@ -1,11 +1,14 @@
 """Module to provide a toolbox with AI algorithms in order to make predictions
 about detecting and couting radon222 marks on images
 """
-from config import *
 import sys
 import os
 from abc import ABC, abstractmethod
-import cv2
+
+import numpy as np
+from config import *
+from IA_ModelsToolBox import utils
+
 class DetCouPredOperations():
     """Call this class to build an interface to prediction operation classes
     """
@@ -14,31 +17,43 @@ class DetCouPredOperations():
     def operation(self,**kwargs):
         pass
 
-class get_radon_image(DetCouPredOperations):
-    """Call this class and return the radon image"""
+class get_plague_image(DetCouPredOperations):
+    """Call this class and return the plague image"""
 
     def operation(self,**kwargs):
         pred_dict = kwargs
-        pred_dict["radon_image_name"] = "testimage1.jpg"
-        #pred_dict["error"] = 1
-        #pred_dict["message"] = "Error en get_radon_img"
-
+        dim = (600,800) #MODIFICAR DE ACUERDO A LO QUE SE CONSIDERE NECESARIO
+        img = utils.read_img(pred_dict["input_img_path"], dim)
+        img = np.expand_dims(img, axis=0) #REVISAR SI ES NECESARIO AGREGAR UNA DIMENSION MAS ej: de 600x800x3 a 1x600x800x3
+        pred_dict["plague_image"] = img
+        
         return pred_dict
 
     def __str__():
         return "radon_img"
+        
+class load_model(DetCouPredOperations):
+    """Load de model..."""
+    
+    def operation(**kwargs):
+        pred_dict = kwargs
+        pred_dict["model"] = modellib.MaskRCNN(mode="inference", 
+                                               config=inference_config,
+                                               model_dir='logs')
+        
+        pred_dict["model"].load_weights(pred_dict["model_path"], by_name=True)
+        
+        return pred_dict
+    
 
 class make_detection_prediction(DetCouPredOperations):
-    model = modellib.MaskRCNN(mode="inference", config=inference_config,  model_dir='logs')
-    def __init__(self):
-        model_path = os.path.join('logs', model_filename)
-        self.model.load_weights(model_filename, by_name=True)
-
-    def operation(self,**kwargs):
+    """ Perform model predictions """
+    
+    def operation(**kwargs):
         pred_dict = kwargs
-        result=self.model.detect([cv2.imread(pred_dict['name'])],verbose=1)
+        result=pred_dict["model"].detect([pred_dict["plague_image"]], verbose=1)
         pred_dict["dete_pred"] = {"pred_1": [1,2,3,4],
-                                  "pred_2": [4,3,2,1],
+                                  "pred_2": [4,3,2,1]
                                  }
         pred_dict["result"]=result[0]
         #pred_dict["error"] = 2
@@ -50,12 +65,11 @@ class make_detection_prediction(DetCouPredOperations):
         return "dete_pred"
 
 class make_couting_operation(DetCouPredOperations):
-    """Call this class to make a couting of radon marks
-    and return a dictionary getting the img, radon222 marks coordinates and
-    the number thereof as well as an error and a message
-    """
+    """Perform plague couting operations"""
 
     def operation(self,**kwargs):
+        #CONSIDERAR AQUI EL PROCESO DE CUENTAS TENIENDO EN CUENTA
+        #LA VARIABLE pred_dict["result"] QUE ESTABLECISTE EN EL PASO ANTERIOR
         pred_dict = kwargs
         pred_dict["coun_pred"] = 20
         #pred_dict["error"] = 3
@@ -69,7 +83,7 @@ class make_couting_operation(DetCouPredOperations):
 class DetCouPredProcedure:
     """Call this class to perform all prediction operations and return a
     dictionary in which there are elements as image path, detection preds,
-    number of radon marks, error and message if necessary 
+    number of plagues, error and message if necessary 
     """
 
     @staticmethod
@@ -85,19 +99,4 @@ class DetCouPredProcedure:
                 break
 
         return predictions_dict
-
-class RadonDetCouModel():
-    """This function should be called from view module to get the prediction
-    dictionary
-    """
-
-    def __init__(self):
-        self.predictions_dict = {}
-
-    def GetRadonDetCou(self, url):
-        """Call this functions to make predictions """
-        predictions = DetCouPredProcedure.call_operations(radon_image_path=url)
-
-        return predictions
-
 
